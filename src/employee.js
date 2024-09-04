c1dd21ede2b84efbfd342dcd5d66f64eb8837dff
Gadrawingz <legmeekmc4@gmail.com>
2024-09-04 22:30:46 +0200

    4th commit on router parameter

--- src/employee.js
+++ src/employee.js
@@ -1,35 +1,54 @@
 // Starting the project real quick
 const express = require('express');
+const req = require('express/lib/request');
 const app = express();
 
 const PORT = 3007;
 
+app.use(express.json());
 app.use(express.urlencoded());
 
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}.`);
 });
 
+const empList = [
+    {
+        names: 'Ashley Cole',
+        salary: 4550000
+    },
+    {
+        names: 'Cody Gakpo',
+        salary: 5320000
+    }, 
+    {
+        names: 'Dwayne Johnson',
+        salary: 750000
+    },
+    {
+        names: 'Bruno Fernandez',
+        salary: 200000
+    },
+];
+
 app.get('/employees', (request, response) => {
-    const empList = [
-        {
-            names: 'Peter Tommy',
-            salary: 400000
-        },
-        {
-            names: 'Cody Gakpo',
-            salary: 5320000
-        }, 
-        {
-            names: 'Daniel Agger',
-            salary: 750000
-        },
-    ];
     response.send(empList);
 });
 
-// Creating a new resource
+// POST Request: Creating a new resource
 app.post('/employees', (request, response) => {
     console.log(request.body);
+    empList.push(request.body)
     response.send(201); // Created
+})
+
+// 05. Dealing with Router parameters
+// Return a single item
+app.get('/employees/:name', (request, response) => {
+    // What's passed here will appear in terminal
+    // console.log(request.params.employee); // Peter
+    const {empName} = request.params;
+    const myEmployee = empList.find((p) => p.name === empName);
+    console.log(request.params.name);
+    response.send(myEmployee);  
 })
